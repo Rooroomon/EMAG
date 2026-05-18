5fcc48d66d89bc73049e2884b9d7ac150cbfb1f2
eGroul <11fbgowns@gmail.com>
2026-05-18 18:52:56 +0900

    second

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,27 +3,45 @@ using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    private PlayerInput playerInput;
+    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] float MoveSpeed = 10;
+    float MoveDir = 0;
+    Rigidbody2D rigid;
 
     void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
-
-        playerInput.onActionTriggered += asd;
+        rigid = GetComponent<Rigidbody2D>();
     }
 
     void OnEnable()
     {
-        
+        playerInput.onActionTriggered += EventSub;
     }
 
-    void Update()
+    void DisEnable()
     {
-        
+        playerInput.onActionTriggered -= EventSub;
     }
 
-    void asd(InputAction.CallbackContext context)
+    void FixedUpdate()
     {
-        print("sdflkjhsdf");
+        rigid.AddForceX(MoveDir * MoveSpeed);
+        rigid.linearVelocityX = Mathf.Clamp(rigid.linearVelocityX, -5, 5);
+    }
+
+    public void EventSub(InputAction.CallbackContext context)
+    {
+        //Debug.Log($"{context.action.name}");
+
+        if(context.action.name == "Move")
+        {
+            MoveDir = context.ReadValue<float>();
+        }
+
+        if(context.action.name == "Jump")
+        {
+            rigid.linearVelocityY = 7.5f;
+        }
     }
 }
